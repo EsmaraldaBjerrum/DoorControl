87c2f81b9b85062ad8100bbc95352fdac10d5677
Sarahkfenger <43434838+Sarahkfenger@users.noreply.github.com>
2019-02-19 13:39:12 +0100

    Tilføjelse af unittest

--- DoorControl.Unit.Test/DoorControlUnitTest.cs
+++ DoorControl.Unit.Test/DoorControlUnitTest.cs
@@ -5,36 +5,52 @@ using System.Text;
 using System.Threading.Tasks;
 using NUnit.Framework;
 using DoorControl;
+using NSubstitute;
 
 namespace DoorControl.Unit.Test
 {
    [TestFixture]
     public class DoorControlUnitTest
     {
-        
+      private DoorControlClass _uut;
+       private IDoor _door;
+       private IAlarm _alarm;
+       private IEntryNotification _entryNotification;
+       private IUserValidation _userValidation;
        
-       private AlarmFake _alarm = new AlarmFake();
-        private DoorControlClass _uut;
-        private DoorFake doorFake;
-        private UserValidationFake userValidationFake;
-
         [SetUp]
         public void SetUp()
         {
-            _uut = new DoorControlClass(_alarm);
-            doorFake = new DoorFake();
-            userValidationFake = new UserValidationFake();
+           _door = Substitute.For<IDoor>();
+           _alarm = Substitute.For<IAlarm>();
+           _entryNotification = Substitute.For<IEntryNotification>();
+           _userValidation = Substitute.For<IUserValidation>();
+            _uut = new DoorControlClass(_alarm,_door,_entryNotification,_userValidation);
+          
         }
      
         [Test]
         public void Alarm_RaiseAlarm_Called()
         {
-            _uut.RaiseAlarm();
-            Assert.That(_alarm.);
+         _uut.RaiseAlarm();
+           _alarm.Received(1).RaiseAlarm();
         }
-   
+
        [Test]
-       public void 
-      
+       public void DoorControl_RequestEntry_EntryGranted()
+       {
+          _userValidation.ValidateEntryRequest("12").Returns(true);
+          _uut.RequestEntry("12");
+          _door.Received(1).Open();
+          _entryNotification.Received(1).NotifyEntryGranted();
+       }
+
+       [Test]
+       public void DoorControl_RequestEntry_EntryDenied()
+       {
+          _userValidation.ValidateEntryRequest("ab").Returns(false);
+          _uut.RequestEntry("ab");
+          _entryNotification.Received(1).NotifyEntryDenied();
+       }
    }
 }
--- DoorControl/DoorControlClass.cs
+++ DoorControl/DoorControlClass.cs
@@ -36,6 +36,8 @@ namespace DoorControl
           {
             _entryNotification.NotifyEntryDenied();
           }
+
+
        }
     }
 }
